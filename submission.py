8fc8b1c7381869243c56756fdb83c95d25fcd015
unknown <salehhsan3@gmail.com>
2023-06-16 09:11:13 +0300

    merged changes

--- submission.py
+++ submission.py
@@ -10,38 +10,27 @@ time_offset = 0.1 # an offset from the time_limit given to our algorithms to ens
 class AgentTurn(Enum):
     MAX = 0
     MIN = 1
+    PROB = 2
 
 # TODO: section a : 3
-
-
-
-
 def smart_heuristic(env: WarehouseEnv, robot_id: int):
     agent: Robot = env.get_robot(robot_id)
     adversary = env.get_robot(1 - robot_id)
-    dist_from_robot = manhattan_distance(agent.position, adversary.position)
     dist = 0
 
     if agent.package is None:
-        if adversary.package is None:
-            dist = min(manhattan_distance(agent.position, env.packages[0].position),
-                       manhattan_distance(agent.position, env.packages[1].position))
-        else:  # adversary is carrying a package, so one package in list of packages
-            for p in env.packages:
-                if p.on_board:
-                    dist = manhattan_distance(p.position, agent.position)
+        dists_from_packs = [manhattan_distance(agent.position, p.position) for p in env.packages if (p.on_board == True)]
+        dist = min(dists_from_packs)
     else:  # agent carries a package
         dist = manhattan_distance(agent.position, agent.package.destination)
+    
+    station_dists = [manhattan_distance(agent.position, station.position) for station in env.charge_stations]
+    closest_station = min(station_dists)
 
-    closest_station = min(manhattan_distance(agent.position, env.charge_stations[0].position),
-                          manhattan_distance(agent.position, env.charge_stations[1].position))
-
-    # try to consider robot's battery: (first try is with: w = 1)
-    w = 1
     if agent.battery <= closest_station:        # once all you can do is get to a charging station, go charge..
         return -1 * (agent.credit + closest_station)
 
-    return (100 * (agent.package is not None)) + 100 * agent.credit - dist
+    return (100 * (agent.package is not None)) + (100 * agent.credit) - dist
 
 
 
@@ -52,6 +41,11 @@ class AgentGreedyImproved(AgentGreedy):
 
 class AgentMinimax(Agent):
     # TODO: section b : 1
+    def heuristic(self, env: WarehouseEnv, robot_id: int):
+        agent = env.get_robot(robot_id)
+        adv = env.get_robot((robot_id + 1)%2)
+        return ( ( agent.credit - adv.credit ) + 0.5 * (agent.battery - adv.battery) )
+
     def compute_next_operation(self, env: WarehouseEnv, agent_id, time_left, turn: AgentTurn, depth):
         start_time = time.time()
         agent = env.get_robot(agent_id)
@@ -61,10 +55,8 @@ class AgentMinimax(Agent):
         
         if (env.num_steps == 0) or (depth == 0):  # game ends when we run out of steps
             if turn == AgentTurn.MIN:
-                agent_id = 1 - agent_id
-            dest_value = temp_heuristic(env, agent_id)
-            # if turn == AgentTurn.MIN:
-            #    dest_value *= (-1)
+                agent_id = (agent_id + 1)%2
+            dest_value = self.heuristic(env, agent_id)
             return dest_value, None
         
         operators = env.get_legal_operators(agent_id)
@@ -82,7 +74,7 @@ class AgentMinimax(Agent):
                     curr_max = result[0]
                     max_op = op 
             return (curr_max, max_op)
-        else:   # turn == AgentTurn.MIN
+        else: # turn == AgentTurn.MIN
             curr_min = np.inf
             min_op = None
             for environment, op in zip(children, operators):
@@ -110,14 +102,141 @@ class AgentMinimax(Agent):
 
 class AgentAlphaBeta(Agent):
     # TODO: section c : 1
+    
+    def heuristic(self, env: WarehouseEnv, robot_id: int):
+        agent = env.get_robot(robot_id)
+        adv = env.get_robot((robot_id + 1)%2)
+        return ( ( agent.credit - adv.credit ) + 0.5 * (agent.battery - adv.battery) )
+    
+    def compute_next_operation( self, env:WarehouseEnv, agent_id, time_left, turn:AgentTurn, depth, alpha, beta):
+        start_time = time.time()
+        
+        if time_left <= time_offset:
+            return (None,None)
+        
+        if (env.num_steps == 0) or (depth == 0):  # game ends when we run out of steps
+            if turn == AgentTurn.MIN:
+                agent_id = (agent_id + 1)%2
+            dest_value = self.heuristic(env, agent_id)
+            return dest_value, None
+        
+        operators = env.get_legal_operators(agent_id)
+        children = [env.clone() for _ in operators]
+        for child, op in zip(children, operators):
+            child.apply_operator(agent_id, op)
+            
+        if turn == AgentTurn.MAX: 
+            max_op = None
+            for environment, op in zip(children, operators):
+                time_left = time_left - (time.time() - start_time)
+                result = self.compute_next_operation(environment,(agent_id+1)%2,time_left, AgentTurn.MIN, depth-1, alpha, beta)
+                if (result[0] != None):
+                    if result[0] >= beta:
+                        return (beta, None) # cut off
+                    elif (result[0] > alpha):
+                        alpha = result[0]
+                        max_op = op 
+            return (alpha, max_op)
+        else: # turn == AgentTurn.MIN
+            min_op = None
+            for environment, op in zip(children, operators):
+                time_left = time_left - (time.time() - start_time)
+                result = self.compute_next_operation(environment,(agent_id+1)%2,time_left, AgentTurn.MAX, depth-1, alpha, beta)
+                if (result[0] != None):
+                    if result[0] <= alpha:
+                        return (alpha, None) # cut off
+                    elif (result[0] < beta):
+                        beta = result[0]
+                        min_op = op
+            return (beta, min_op)
+    
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        raise NotImplementedError()
+        max_depth = env.num_steps
+        depth = 1
+        start_time = time.time()
+        operation = None
+        alpha = -(np.inf)
+        beta = np.inf
+        while ( ( (time.time() - start_time) < time_offset ) and (depth <= max_depth) ):
+            result = self.compute_next_operation(env,agent_id, (time_limit - (time.time() - start_time)), AgentTurn.MAX, depth, alpha, beta )
+            if result[1] is not None:
+                operation = result[1]
+            else: # ran out of time
+                break 
+            depth += 1
+        return operation
 
 
 class AgentExpectimax(Agent):
     # TODO: section d : 1
+    def heuristic(self, env: WarehouseEnv, robot_id: int):
+        agent = env.get_robot(robot_id)
+        adv = env.get_robot((robot_id + 1)%2)
+        return ( ( agent.credit - adv.credit ) + 0.5 * (agent.battery - adv.battery) )
+    
+    def compute_next_operation( self, env:WarehouseEnv, agent_id, time_left, turn:AgentTurn, depth, last_caller:AgentTurn = None):
+        start_time = time.time()
+        
+        if time_left <= time_offset:
+            return (None,None)
+        
+        if (env.num_steps == 0) or (depth == 0):  # game ends when we run out of steps
+            if turn == AgentTurn.MIN:
+                agent_id = (agent_id + 1)%2
+            dest_value = self.heuristic(env, agent_id)
+            return dest_value, None
+        
+        operators = env.get_legal_operators(agent_id)
+        children = [env.clone() for _ in operators]
+        for child, op in zip(children, operators):
+            child.apply_operator(agent_id, op)
+        
+        if turn == AgentTurn.PROB:
+            expected = 0
+            probability = (1 / len(operators))
+            for environment, op in zip(children, operators):
+                time_left = time_left - (time.time() - start_time)
+                if last_caller == AgentTurn.MAX:
+                    result = self.compute_next_operation(environment,(agent_id+1)%2,time_left, AgentTurn.MIN, depth-1, AgentTurn.PROB)
+                elif last_caller == AgentTurn.MIN:
+                    result = self.compute_next_operation(environment,(agent_id+1)%2,time_left, AgentTurn.MAX, depth-1, AgentTurn.PROB)
+                if result[0] != None:
+                    expected += (probability * result[0])
+            return (expected, None)
+        
+        elif turn == AgentTurn.MAX: 
+            max_op = None
+            curr_max = -(np.inf)
+            for environment, op in zip(children, operators):
+                time_left = time_left - (time.time() - start_time)
+                result = self.compute_next_operation(environment,(agent_id+1)%2,time_left, AgentTurn.PROB, depth-1, AgentTurn.MAX)
+                if (result[0] != None) and ((result[0] > curr_max)):
+                        curr_max = result[0]
+                        max_op = op 
+            return (curr_max, max_op)
+        elif turn == AgentTurn.MIN: 
+            min_op = None
+            for environment, op in zip(children, operators):
+                time_left = time_left - (time.time() - start_time)
+                result = self.compute_next_operation(environment,(agent_id+1)%2,time_left, AgentTurn.PROB, depth-1, AgentTurn.MIN)
+                if (result[0] != None):
+                        curr_min = result[0]
+                        min_op = op
+            return (curr_min, min_op)
+        
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        raise NotImplementedError()
+        max_depth = env.num_steps
+        depth = 1
+        start_time = time.time()
+        operation = None
+        while ( ( (time.time() - start_time) < time_offset ) and (depth <= max_depth) ):
+            result = self.compute_next_operation(env,agent_id, (time_limit - (time.time() - start_time)), AgentTurn.MAX, depth, None )
+            if result[1] is not None:
+                operation = result[1]
+            else: # ran out of time
+                break 
+            depth += 1
+        return operation
 
 
 # here you can check specific paths to get to know the environment
@@ -142,5 +261,3 @@ class AgentHardCoded(Agent):
         operators, _ = self.successors(env, robot_id)
 
         return random.choice(operators)
-    
-    
